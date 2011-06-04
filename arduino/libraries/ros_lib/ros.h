aa97cddc47a19c039f7512c1d847632acdba1696
Michael Ferguson <mferguson@willowgarage.com>
2011-06-03 17:31:05 -0700

    Import ros_lib library

---
+++ arduino/libraries/ros_lib/ros.h
@@ -0,0 +1,130 @@
+/* 
+ * Software License Agreement (BSD License)
+ *
+ * Copyright (c) 2011, Willow Garage, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials provided
+ *    with the distribution.
+ *  * Neither the name of Willow Garage, Inc. nor the names of its
+ *    contributors may be used to endorse or promote prducts derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* 
+ * ROS definitions for Arduino
+ * Author: Michael Ferguson
+ */
+
+#ifndef ros_lib_h
+#define ros_lib_h
+
+#define MAX_PUBLISHERS      25
+#define MAX_SUBSCRIBERS     25
+
+#define BUFFER_SIZE         512
+
+namespace ros
+{
+  class NodeHandle;
+
+  /* Base Message Type */
+  class Msg
+  {
+    public: 
+      virtual int serialize(unsigned char *outbuffer) = 0;
+	  virtual int deserialize(unsigned char *data) = 0;
+      virtual const char * getType() = 0;
+      
+  };
+
+  typedef void (msgCb) (unsigned char * data);
+
+  /* Generic Publisher */
+  class Publisher
+  {
+    public:
+      Publisher( const char * topic_name, Msg * msg );
+      int publish( Msg * msg );
+
+      int id_;     
+      NodeHandle * nh_;
+      const char * topic_;
+      Msg * msg_;
+  };
+
+  /* Generic Subscriber */
+  class Subscriber
+  {
+    public:
+      Subscriber( const char * topic_name, Msg * msg, msgCb * callback);
+
+      int id_;     
+      NodeHandle * nh_;
+      const char * topic_;
+      Msg * msg_;
+      msgCb * cb_;
+  };
+
+  /* Node Handle */
+  class NodeHandle
+  {
+    public:
+      bool advertise(Publisher &p);
+      bool subscribe(Subscriber &s);
+      void negotiateTopics();
+      int publish(int id, Msg * msg);
+
+      /* Start serial, initialize buffers */
+      void initNode();
+
+      /* This function goes in your loop() function, it handles 
+       *  serial input and callbacks for subscribers. 
+       */
+      void spinOnce();
+
+      bool configured_;
+
+    private:
+      void makeHeader();
+      Publisher * publishers[MAX_PUBLISHERS];
+      Subscriber * subscribers[MAX_SUBSCRIBERS];
+
+      int mode_;
+      int bytes_;
+      int topic_;
+      int index_;
+
+      unsigned char message_in[BUFFER_SIZE];
+      unsigned char message_out[BUFFER_SIZE];
+  };
+
+}
+
+#define ROS_CALLBACK( fn, ty, message ) \ 
+ty message; \
+void fn (unsigned char *data){ \
+    message.deserialize(data); 
+
+#endif
---
+++ arduino/libraries/ros_lib/ros_lib.cpp
@@ -0,0 +1,239 @@
+/* 
+ * Software License Agreement (BSD License)
+ *
+ * Copyright (c) 2011, Willow Garage, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials provided
+ *    with the distribution.
+ *  * Neither the name of Willow Garage, Inc. nor the names of its
+ *    contributors may be used to endorse or promote prducts derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* 
+ * Author: Michael Ferguson
+ */
+
+#include "ros.h"
+#include "WProgram.h"
+
+#define MODE_FIRST_FF       0
+#define MODE_SECOND_FF      1
+#define MODE_TOPIC          2   // waiting for topic id
+#define MODE_TYPE           3   // waiting for topic type
+#define MODE_SIZE_H         4   // waiting for message size
+#define MODE_SIZE_L         5   
+#define MODE_MESSAGE        6
+
+/* 
+ * Publishers 
+ */
+ros::Publisher::Publisher(const char * topic_name, Msg * msg )
+{
+  id_ = 0;
+  topic_ = topic_name;
+  msg_ = msg;
+}
+
+int ros::Publisher::publish(Msg * msg)
+{
+  if(!nh_->configured_)
+    return 0;
+  return nh_->publish(id_, msg);
+}
+
+
+/* 
+ * Subscribers
+ */
+ros::Subscriber::Subscriber(const char * topic_name, Msg * msg, msgCb *callback)
+{
+  id_ = 0;
+  topic_ = topic_name;
+  msg_ = msg;
+  cb_ = callback; 
+}
+
+
+/* 
+ * Node Handles
+ */
+bool ros::NodeHandle::advertise(Publisher & p)
+{
+  int i;
+  for(i = 0; i < MAX_PUBLISHERS; i++)
+  {
+    if(publishers[i] == 0) // empty slot
+    {
+      publishers[i] = &p;
+      p.id_ = i+1;
+      p.nh_ = this;
+      return true;
+    }
+  }
+  return false;
+}
+
+bool ros::NodeHandle::subscribe(Subscriber & s)
+{
+  int i;
+  for(i = 0; i < MAX_SUBSCRIBERS; i++)
+  {
+    if(subscribers[i] == 0) // empty slot
+    {
+      subscribers[i] = &s;
+      s.id_ = i+128;
+      s.nh_ = this;
+      return true;
+    }
+  }
+  return false;
+}
+
+void ros::NodeHandle::negotiateTopics()
+{
+  int i;
+  for(i = 0; i < MAX_PUBLISHERS; i++)
+  {
+    if(publishers[i] != 0) // non-empty slot
+    {
+      // send publisher information
+      makeHeader();
+      Serial.write( (unsigned char) 0 );
+      Serial.write( strlen(publishers[i]->topic_) + strlen(publishers[i]->msg_->getType()) + 3 );
+      Serial.write( publishers[i]->id_ );  
+      Serial.write( strlen(publishers[i]->topic_) );
+      Serial.print( publishers[i]->topic_ );
+      Serial.write( strlen(publishers[i]->msg_->getType()) );
+      Serial.print( publishers[i]->msg_->getType() );
+    }
+  }
+  for(i = 0; i < MAX_SUBSCRIBERS; i++)
+  {
+    if(subscribers[i] != 0) // non-empty slot
+    {
+      // send subscriber information
+      makeHeader();
+      Serial.write( (unsigned char) 0 );
+      Serial.write( strlen(subscribers[i]->topic_) + strlen(subscribers[i]->msg_->getType()) + 3 );
+      Serial.write( subscribers[i]->id_ );  
+      Serial.write( strlen(subscribers[i]->topic_) );
+      Serial.print( subscribers[i]->topic_ );
+      Serial.write( strlen(subscribers[i]->msg_->getType()) );
+      Serial.print( subscribers[i]->msg_->getType() );
+    }
+  }
+  configured_ = true;
+}
+
+int ros::NodeHandle::publish(int id, Msg * msg)
+{
+  int l = msg->serialize(message_out);
+  makeHeader();
+  Serial.write(id);
+  Serial.write( (unsigned char) 0 );
+  Serial.write(l);
+  int i;
+  for( i = 0; i<l; i++)
+  {
+    Serial.write(message_out[i]);
+  }
+  return 1;
+}
+
+inline void ros::NodeHandle::makeHeader()
+{
+  Serial.write(0xff);
+  Serial.write(0xff);
+}
+
+void ros::NodeHandle::initNode()
+{
+  // initialize publisher and subscriber lists
+  int i;
+  for(i = 0; i < MAX_PUBLISHERS; i++)
+  {
+    publishers[i] = 0;
+    subscribers[i] = 0;
+  }
+  // initialize serial
+  Serial.begin(57600);
+  configured_ = false;
+  mode_ = 0;
+  bytes_ = 0;
+  index_ = 0;
+  topic_ = 0;
+}
+
+void ros::NodeHandle::spinOnce()
+{
+  /* while available buffer, read data */
+  while( Serial.available() > 0 )
+  {  
+    int data = Serial.read();
+    if( mode_ == MODE_MESSAGE ){        /* message data being recieved */
+      message_in[index_++] = data;
+      bytes_--;
+      if(bytes_ == 0)                   /* is message complete? if so, pass along */
+      {
+        if(topic_ == 0)
+        {
+          negotiateTopics();
+          Serial.flush();
+        }
+        else
+        {
+          if(subscribers[topic_-128])
+            subscribers[topic_-128]->cb_( message_in );
+        }
+        mode_ = 0;
+      }
+    }else if( mode_ == MODE_FIRST_FF ){
+      if(data == 0xff)
+        mode_++;
+    }else if( mode_ == MODE_SECOND_FF ){
+      if(data == 0xff)
+        mode_++;
+      else
+        mode_ = MODE_FIRST_FF;
+    }else if( mode_ == MODE_TOPIC ){    /* this is topic name */
+      topic_ = data;
+      if(topic_ == 0 || topic_ > 127)
+        mode_++;
+      else
+        mode_ = MODE_FIRST_FF;
+    }else if( mode_ == MODE_TYPE ){     /* this is the message type (topic or service) */
+      mode_++;
+    }else if( mode_ == MODE_SIZE_H ){   /* top half of message size */
+      bytes_ = data<<8;
+      mode_++;
+    }else if( mode_ == MODE_SIZE_L ){   /* bottom half of message size */
+      bytes_ += data;
+      index_ = 0;
+      mode_++;
+    }
+  }
+}
+
