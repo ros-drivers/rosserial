876c62f2214bd182fcdaf4cc370d19e899d1dcd2
Michael Ferguson <mferguson@willowgarage.com>
2012-03-30 09:45:08 -0700

    Apply part of #49

--- rosserial_arduino/src/ros_lib/ArduinoHardware.h
+++ rosserial_arduino/src/ros_lib/ArduinoHardware.h
@@ -35,7 +35,11 @@
 #ifndef ROS_ARDUINO_HARDWARE_H_
 #define ROS_ARDUINO_HARDWARE_H_
 
-#include "WProgram.h"
+#if ARDUINO>=100
+#include <Arduino.h> // Arduino 1.0
+#else
+#include <Wprogram.h> // Arduino 0022
+#endif
 #include <HardwareSerial.h>
 
 class ArduinoHardware {
