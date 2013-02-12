4b7f2f37c4876ab61a8409810187b381ba7062d1
Mike Ferguson <mike@vanadiumlabs.com>
2013-02-12 01:30:15 -0800

    fix massive bug in message generation

--- rosserial_arduino/src/rosserial_arduino/make_libraries.py
+++ rosserial_arduino/src/rosserial_arduino/make_libraries.py
@@ -86,5 +86,5 @@ rosserial_arduino_dir = rospack.get_path(THIS_PACKAGE)
 shutil.copytree(rosserial_arduino_dir+"/src/ros_lib", path+"/ros_lib")
 
 # generate messages
-rosserial_generate(rospack, path)
+rosserial_generate(rospack, path, ROS_TO_EMBEDDED_TYPES)
 
