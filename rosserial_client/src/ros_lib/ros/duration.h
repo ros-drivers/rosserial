516352cc03f62b6b01b2ff85e8abcfb4ccac84b6
Mike Ferguson <mike@vanadiumlabs.com>
2013-02-13 02:28:33 -0800

    close #6

--- rosserial_client/src/ros_lib/ros/duration.h
+++ rosserial_client/src/ros_lib/ros/duration.h
@@ -50,6 +50,9 @@ namespace ros {
         normalizeSecNSecSigned(sec, nsec);
       }
 
+      double toSec() const { return (double)sec + 1e-9*(double)nsec; };
+      void fromSec(double t) { sec = (unsigned long) floor(t); nsec = (unsigned long) round((t-sec) * 1e9); };
+
       Duration& operator+=(const Duration &rhs);
       Duration& operator-=(const Duration &rhs);
       Duration& operator*=(double scale);
