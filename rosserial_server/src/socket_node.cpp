6fd6a9c466fa754b7fb257b7f197d02588ad2098
Mike Purvis <mpurvis@clearpathrobotics.com>
2013-07-13 09:21:04 -0400

    Reading and displaying TopicTools message from client.

--- rosserial_server/src/socket_node.cpp
+++ rosserial_server/src/socket_node.cpp
@@ -1,8 +1,13 @@
 #include <cstdlib>
 #include <iostream>
+#include <tr1/unordered_map>
 #include <boost/bind.hpp>
 #include <boost/asio.hpp>
 #include <boost/function.hpp>
+
+#include <ros/ros.h>
+#include <rosserial_msgs/TopicInfo.h>
+#include <topic_tools/shape_shifter.h>
 
 using boost::asio::ip::tcp;
 
@@ -15,7 +20,7 @@ public:
     mem_.resize(capacity);
   }
 
-  void read(size_t read_count, boost::function<void(char*)> callback) {
+  void read(size_t read_count, boost::function<void(ros::serialization::IStream)> callback) {
     // Objective is to ensure that the buffer contains number of bytes,
     // contiguously, that the caller wishes.
     // If it does already, then call the callback immediately. If not, then
@@ -38,21 +43,17 @@ public:
 
 private:
   void read_cb(const boost::system::error_code& error, size_t bytes_transferred,
-               boost::function<void(char*)> callback, size_t read_count) {
-    if (!error) {
+               boost::function<void(ros::serialization::IStream)> callback, size_t read_count) {
+    if (error) {
       // TODO: handle error code.
-
+    } else {
       size_ += bytes_transferred;
 
       if (callback) {
-        size_t read = start_;
+        ros::serialization::IStream stream(&mem_[start_], read_count);
         start_ += read_count;
         size_ -= read_count;
-        if (size_ == 0) {
-          start_ = 0;
-        }
-        //printf("[%ld,%ld]", start_, size_);
-        callback(&mem_[read]);
+        callback(stream);
       } else {
         // TODO: handle bad callback.
       }
@@ -65,7 +66,7 @@ private:
 
   AsyncReadStream& stream_;
 
-  std::vector<char> mem_;
+  std::vector<uint8_t> mem_;
 
   size_t start_;  // Index of next char to be removed/returned
   size_t size_;  // Number of bytes in buffer at present.
@@ -75,8 +76,10 @@ class Session
 {
 public:
   Session(boost::asio::io_service& io_service)
-    : socket_(io_service), buffer_(socket_, 1024)
+    : socket_(io_service), buffer_(socket_, buffer_max)
   {
+    //callbacks.insert(std::pair0, boost::bind(&Session::setup_publisher, this)}})
+    callbacks[0] = boost::bind(&Session::setup_publisher, this, _1);
   }
 
   tcp::socket& socket()
@@ -87,46 +90,82 @@ public:
   void start()
   {
     std::cout << "Starting\n";
-    const char* request_topics = "\xff\xff\x00\x00\x00\x00\xff";
-    boost::asio::async_write(socket_, boost::asio::buffer(request_topics, sizeof(request_topics)),
-          boost::bind(&Session::handle_write, this, boost::asio::placeholders::error));
+
+
+
+    request_topics();
     buffer_.read(1, boost::bind(&Session::read_sync_first, this, _1));
   }
 
 private:
-  void read_sync_first(char* buffer_ptr) {
-    printf("x %d \n", buffer_ptr[0]);
-    if (buffer_ptr[0] == -1) {
+  void request_topics() {
+    const char* msg = (char*)"\xff\xff\x00\x00\x00\x00\xff";
+    boost::asio::async_write(socket_, boost::asio::buffer(msg, sizeof(msg)),
+          boost::bind(&Session::write_cb, this, boost::asio::placeholders::error));
+  }
+
+  void read_sync_first(ros::serialization::IStream stream) {
+    uint8_t sync;
+    stream >> sync;
+    if (sync == 0xff) {
       buffer_.read(1, boost::bind(&Session::read_sync_second, this, _1));
     } else {
       buffer_.read(1, boost::bind(&Session::read_sync_first, this, _1));
     }
   }
   
-  void read_sync_second(char* buffer_ptr) {
-    if (buffer_ptr[0] == -1) {
+  void read_sync_second(ros::serialization::IStream stream) {
+    uint8_t sync;
+    stream >> sync;
+    if (sync == 0xff) {
       buffer_.read(4, boost::bind(&Session::read_id_length, this, _1));
     } else {
       buffer_.read(1, boost::bind(&Session::read_sync_first, this, _1));
     }
   }
 
-  void read_id_length(char* buffer_ptr) {
-    uint16_t topic_id = *(uint16_t*)(buffer_ptr + 0);
-    uint16_t length = *(uint16_t*)(buffer_ptr + 2);
-    printf("rx %d %d\n", topic_id, length);
+  void read_id_length(ros::serialization::IStream stream) {
+    uint16_t topic_id, length;
+    stream >> topic_id >> length;
+    // printf("rx %d %d\n", topic_id, length);
+    // TODO: Check length against expected for fixed-length messages.
 
     buffer_.read(length + 1, boost::bind(&Session::read_message, this,
-                                         _1, topic_id, length));
+                                         _1, topic_id));
   }
 
-  void read_message(char* buffer_ptr, uint16_t topic_id, uint16_t length) {
+  void read_message(ros::serialization::IStream stream, uint16_t topic_id) {
+    // TODO: Check checksum here.
+    
+    if (callbacks.count(topic_id) == 1) {
+      callbacks[topic_id](stream);
+    } else {
+      // TODO: Handle unrecognized topic_id.
+    }
+
+    // Kickoff next message read.
     buffer_.read(1, boost::bind(&Session::read_sync_first, this, _1));
+  }
+
+  void write_cb(const boost::system::error_code& error) { 
+    if (error) {
+      delete this;
+    }
+  }
+
+  void setup_publisher(ros::serialization::IStream& stream) {
+    rosserial_msgs::TopicInfo msg;
+    ros::serialization::Serializer<rosserial_msgs::TopicInfo>::read(stream, msg);
+    std::cout << msg;
+    //publishers[] =
+    //callback[] = 
   }
 
   tcp::socket socket_;
   AsyncReadBuffer<tcp::socket> buffer_;
   enum { buffer_max = 1023 };
+
+  std::tr1::unordered_map< uint16_t, boost::function<void(ros::serialization::IStream)> > callbacks;
 };
 
 class Server
