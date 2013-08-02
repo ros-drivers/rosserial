256dd3132193e061e3ac0544b49418ce779e1a6b
Mike Purvis <mpurvis@clearpathrobotics.com>
2013-08-02 15:59:53 -0400

    Stub for a message info service.

--- rosserial_python/nodes/message_info_service.py
+++ rosserial_python/nodes/message_info_service.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python
 
 import rospy
+from rosserial_msgs import RequestMessageInfo
+from rosserial_python import load_message
+
+
+def cb(req):  
+
+if __name__=="__main__":
+    rospy.init_node("message_info_service")
+    rospy.loginfo("rosserial message_info_service node")
+    rospy.Server("message_info", RequestMessageInfo, cb)
+    rospy.spin()
