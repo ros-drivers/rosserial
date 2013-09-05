00c752067ad54e031715df706a91e86568ef051b
Chuck <cfharr@gmail.com>
2013-09-04 21:36:20 -0700

    add simplified sprintf function

---
+++ rosserial_psoc4/src/ros_lib/isnprintf.h
@@ -0,0 +1,12 @@
+/******************************************************************************
+*This file is for iprintf()
+*******************************************************************************/
+#ifndef ISNPRINTF_H
+#define ISNPRINTF_H
+#include <inttypes.h>
+#include <cstddef>
+
+int isnprintf(char *s, int buf_len, char *pszFmt,...);
+
+#endif
+/* [] END OF FILE */
