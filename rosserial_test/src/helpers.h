c91cb16edd2a58f1c8673ecdd7ea2320885b9a5d
Mike Purvis <mpurvis@clearpathrobotics.com>
2014-06-17 16:36:33 -0400

    Add single subscribe test.

--- rosserial_test/src/helpers.h
+++ rosserial_test/src/helpers.h
@@ -2,11 +2,16 @@
 template<class Msg>
 class Callback {
 public:
-  Callback() : times_called(0) {}
-  void callback(const Msg msg) {
+  Callback() : times_called(0)
+  {
+  }
+
+  void callback(const Msg msg)
+  {
     times_called++;
     last_msg = msg;
   }
+
   Msg last_msg;
   int times_called;
 };
