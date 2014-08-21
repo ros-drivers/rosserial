b52cf3f96939d29c8e72dbff5df1590b6e5002a8
Mike Purvis <mpurvis@clearpathrobotics.com>
2014-08-21 12:05:07 -0400

    Remove difftime call from rosserial_client

--- rosserial_client/src/ros_lib/ros/node_handle.h
+++ rosserial_client/src/ros_lib/ros/node_handle.h
@@ -211,7 +211,7 @@ namespace ros {
               mode_++;
               last_msg_timeout_time = c_time + MSG_TIMEOUT;
             }
-            else if( difftime(hardware_.time(), c_time) > (SYNC_SECONDS)){
+            else if( hardware_.time() - c_time > (SYNC_SECONDS)){
               /* We have been stuck in spinOnce too long, return error */
               configured_=false;
               return -2;
