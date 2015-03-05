663459f4f74bf0c6d37a8de5251f0a3882345082
Tony Baltovski <tony.baltovski@gmail.com>
2015-03-04 22:55:30 -0500

    Fixed typo.

--- rosserial_client/src/ros_lib/time.cpp
+++ rosserial_client/src/ros_lib/time.cpp
@@ -38,7 +38,7 @@ namespace ros
 {
   void normalizeSecNSec(uint32_t& sec, uint32_t& nsec){
     uint32_t nsec_part= nsec % 1000000000UL;
-    unint32_t sec_part = nsec / 1000000000UL;
+    uint32_t sec_part = nsec / 1000000000UL;
     sec += sec_part;
     nsec = nsec_part;
   }
