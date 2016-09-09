0c8c5b6369b82a5865e8c9287c2c7b0a764f8383
Mike Purvis <mike@uwmike.com>
2016-09-08 20:43:18 -0400

    Fix AsyncReadBuffer for UDP socket case. (#245)

--- rosserial_server/include/rosserial_server/serial_session.h
+++ rosserial_server/include/rosserial_server/serial_session.h
@@ -69,7 +69,7 @@ private:
     // if the ROS node is still up.
     if (ros::ok())
     {
-      timer_.expires_from_now(boost::posix_time::milliseconds(1000));
+      timer_.expires_from_now(boost::posix_time::milliseconds(2000));
       timer_.async_wait(boost::bind(&SerialSession::check_connection, this));
     }
   }
