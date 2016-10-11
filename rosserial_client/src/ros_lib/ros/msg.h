0fe953c2c62b82825054c5cb21ce7a176a237741
Ivan Feofanov <raketomania@yandex.ru>
2016-10-11 19:58:12 +0300

    Added <stddef.h> for size_t

--- rosserial_client/src/ros_lib/ros/msg.h
+++ rosserial_client/src/ros_lib/ros/msg.h
@@ -36,6 +36,7 @@
 #define _ROS_MSG_H_
 
 #include <stdint.h>
+#include <stddef.h>
 
 namespace ros {
 
@@ -122,6 +123,7 @@ public:
     return 8;
   }
 
+  // Copy data from variable into a byte array
   template<typename A, typename V>
   static void varToArr(A arr, const V var)
   {
@@ -129,6 +131,7 @@ public:
       arr[i] = (var >> (8 * i));
   }
 
+  // Copy data from a byte array into variable
   template<typename V, typename A>
   static void arrToVar(V& var, const A arr)
   {
