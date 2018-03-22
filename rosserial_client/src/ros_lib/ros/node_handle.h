8da1de4621cbb977750ae2649ef8aa252a14644d
Carl Moser <carl.moser@students.olin.edu>
2018-03-22 10:46:35 -0400

    Adding suppport for subsciber to call member functions (#307)

--- rosserial_client/src/ros_lib/ros/node_handle.h
+++ rosserial_client/src/ros_lib/ros/node_handle.h
@@ -348,11 +348,11 @@ namespace ros {
       }
 
       /* Register a new subscriber */
-      template<typename MsgT>
-      bool subscribe(Subscriber< MsgT> & s){
+      template<typename SubscriberT>
+      bool subscribe(SubscriberT& s){
         for(int i = 0; i < MAX_SUBSCRIBERS; i++){
           if(subscribers[i] == 0){ // empty slot
-            subscribers[i] = (Subscriber_*) &s;
+            subscribers[i] = static_cast<Subscriber_*>(&s);
             s.id_ = i+100;
             return true;
           }
