59925faaa4d246ab4d37ce76d4763a5112d97de7
chrisspen <chrisspen@gmail.com>
2018-03-23 13:34:50 -0400

    Removed pep8 validation files. Updated cmakelists and package.xml to include dependencies. Removed unnecessary tcp functionality from arduino-specific serial_node.py

--- pep8.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-pylint --rcfile=pylint.rc rosserial_python/src/rosserial_python rosserial_arduino/src/rosserial_arduino/SerialClient.py
--- rosserial_arduino/nodes/serial_node.py
+++ rosserial_arduino/nodes/serial_node.py
@@ -35,10 +35,8 @@
 
 import rospy
 from rosserial_arduino import SerialClient
-from rosserial_python import RosSerialServer
 from serial import SerialException
 from time import sleep
-import multiprocessing
 
 import sys
 
@@ -58,43 +56,21 @@ if __name__=="__main__":
     # TIOCM_DTR_str) line, which causes an IOError, when using simulated port
     fix_pyserial_for_test = rospy.get_param('~fix_pyserial_for_test', False)
 
-    # TODO: should these really be global?
-    tcp_portnum = int(rospy.get_param('/rosserial_embeddedlinux/tcp_port', '11411'))
-    fork_server = rospy.get_param('/rosserial_embeddedlinux/fork_server', False)
-
     # TODO: do we really want command line params in addition to parameter server params?
     sys.argv = rospy.myargv(argv=sys.argv)
     if len(sys.argv) >= 2 :
         port_name  = sys.argv[1]
-    if len(sys.argv) == 3 :
-        tcp_portnum = int(sys.argv[2])
 
-    if port_name == "tcp" :
-        server = RosSerialServer(tcp_portnum, fork_server)
-        rospy.loginfo("Waiting for socket connections on port %d" % tcp_portnum)
+    while not rospy.is_shutdown():
+        rospy.loginfo("Connecting to %s at %d baud" % (port_name, baud))
         try:
-            server.listen()
+            client = SerialClient(port_name, baud, fix_pyserial_for_test=fix_pyserial_for_test, auto_reset_timeout=auto_reset_timeout)
+            client.run()
         except KeyboardInterrupt:
-            rospy.loginfo("got keyboard interrupt")
-        finally:
-            rospy.loginfo("Shutting down")
-            for process in multiprocessing.active_children():
-                rospy.loginfo("Shutting down process %r", process)
-                process.terminate()
-                process.join()
-            rospy.loginfo("All done")
-
-    else :          # Use serial port
-        while not rospy.is_shutdown():
-            rospy.loginfo("Connecting to %s at %d baud" % (port_name,baud) )
-            try:
-                client = SerialClient(port_name, baud, fix_pyserial_for_test=fix_pyserial_for_test, auto_reset_timeout=auto_reset_timeout)
-                client.run()
-            except KeyboardInterrupt:
-                break
-            except SerialException:
-                sleep(1.0)
-                continue
-            except OSError:
-                sleep(1.0)
-                continue
+            break
+        except SerialException:
+            sleep(1.0)
+            continue
+        except OSError:
+            sleep(1.0)
+            continue
