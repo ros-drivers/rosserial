0a955002040f007a4a525850b962dbc9c2d6eb4e
cannon <cannonator007@gmail.com>
2018-06-29 15:06:43 -0700

    initial commit of Cortex support

---
+++ rosserial_cortex/nodes/serial_node.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+#####################################################################
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2011, Willow Garage, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of Willow Garage, Inc. nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import rospy
+from rosserial_arduino import SerialClient
+from serial import SerialException
+from time import sleep
+
+import sys
+
+if __name__=="__main__":
+
+    rospy.init_node("serial_node")
+    rospy.loginfo("ROS Serial Python Node")
+
+    port_name = rospy.get_param('~port','/dev/ttyUSB0')
+    baud = int(rospy.get_param('~baud','57600'))
+
+    # Number of seconds of sync failure after which Arduino is auto-reset.
+    # 0 = no timeout, auto-reset disabled
+    auto_reset_timeout = int(rospy.get_param('~auto_reset_timeout','0'))
+
+    # for systems where pyserial yields errors in the fcntl.ioctl(self.fd, TIOCMBIS, \
+    # TIOCM_DTR_str) line, which causes an IOError, when using simulated port
+    fix_pyserial_for_test = rospy.get_param('~fix_pyserial_for_test', False)
+
+    # TODO: do we really want command line params in addition to parameter server params?
+    sys.argv = rospy.myargv(argv=sys.argv)
+    if len(sys.argv) >= 2 :
+        port_name  = sys.argv[1]
+
+    while not rospy.is_shutdown():
+        rospy.loginfo("Connecting to %s at %d baud" % (port_name, baud))
+        try:
+            client = SerialClient(port_name, baud, fix_pyserial_for_test=fix_pyserial_for_test, auto_reset_timeout=auto_reset_timeout)
+            client.run()
+        except KeyboardInterrupt:
+            break
+        except SerialException:
+            sleep(1.0)
+            continue
+        except OSError:
+            sleep(1.0)
+            continue
