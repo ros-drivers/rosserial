a6c3f6942edf1bf2f01476b637755a1750f908f6
supermhel <45690192+supermhel@users.noreply.github.com>
2019-02-12 14:07:46 +0100

    Update ArduinoHardware.h
    i want to try a new baudrate to see if i obtain different results from this

--- rosserial_arduino/src/ros_lib/ArduinoHardware.h
+++ rosserial_arduino/src/ros_lib/ArduinoHardware.h
@@ -64,7 +64,7 @@
 
 class ArduinoHardware {
   public:
-    ArduinoHardware(SERIAL_CLASS* io , long baud= 57600){
+    ArduinoHardware(SERIAL_CLASS* io , long baud= 115200){
       iostream = io;
       baud_ = baud;
     }
@@ -78,7 +78,7 @@ class ArduinoHardware {
 #else
       iostream = &Serial;
 #endif
-      baud_ = 57600;
+      baud_ = 115200;
     }
     ArduinoHardware(ArduinoHardware& h){
       this->iostream = h.iostream;
