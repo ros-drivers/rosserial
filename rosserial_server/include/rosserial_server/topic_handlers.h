d636dbbe05043e28e4b0bf682620355e1c6c8c9a
Moju Zhao <chou@jsk.imi.i.u-tokyo.ac.jp>
2019-11-21 08:59:13 +0900

    Add ServiceServer in rosserial server using topic_tools::ShapeShifter.

--- rosserial_server/include/rosserial_server/topic_handlers.h
+++ rosserial_server/include/rosserial_server/topic_handlers.h
@@ -190,6 +190,108 @@ private:
 
 typedef boost::shared_ptr<ServiceClient> ServiceClientPtr;
 
+
+class ServiceServer {
+public:
+  ServiceServer(ros::NodeHandle& nh, rosserial_msgs::TopicInfo& topic_info,
+      boost::function<void(std::vector<uint8_t>& buffer, const uint16_t topic_id)> write_fn)
+    : write_fn_(write_fn) {
+    topic_id_ = -1;
+    get_response_ = false;
+
+    rosserial_server::MsgInfo srvinfo;
+    rosserial_server::MsgInfo reqinfo;
+    rosserial_server::MsgInfo respinfo;
+    try
+    {
+      srvinfo = lookupMessage(topic_info.message_type, "srv");
+      reqinfo = lookupMessage(topic_info.message_type + "Request", "srv");
+      respinfo = lookupMessage(topic_info.message_type + "Response", "srv");
+    }
+    catch (const std::exception& e)
+    {
+      ROS_WARN_STREAM("Unable to look up service definition: " << e.what());
+    }
+
+    service_md5_ = srvinfo.md5sum;
+    request_message_md5_ = reqinfo.md5sum;
+    response_message_md5_ = respinfo.md5sum;
+
+    ros::AdvertiseServiceOptions opts;
+    opts.service = topic_info.topic_name;
+    opts.md5sum = service_md5_;
+    opts.datatype = topic_info.message_type;
+    opts.req_datatype = topic_info.message_type + "Request";
+    opts.res_datatype = topic_info.message_type + "Response";
+    opts.helper = boost::make_shared<ros::ServiceCallbackHelperT<ros::ServiceSpec<topic_tools::ShapeShifter,topic_tools::ShapeShifter> > >(boost::bind(&ServiceServer::request_handle, this, _1, _2));
+
+    service_server_ = nh.advertiseService(opts);
+  }
+  void setTopicId(uint16_t topic_id) {
+    topic_id_ = topic_id;
+  }
+  std::string getServiceMD5() {
+    return service_md5_;
+  }
+  std::string getRequestMessageMD5() {
+    return request_message_md5_;
+  }
+  std::string getResponseMessageMD5() {
+    return response_message_md5_;
+  }
+
+  bool request_handle(topic_tools::ShapeShifter& request_message, topic_tools::ShapeShifter& response_message) {
+
+    get_response_ = false;
+    ROS_DEBUG_STREAM("service receive " << service_server_.getService() << " request");
+
+    size_t length = ros::serialization::serializationLength(request_message);
+    std::vector<uint8_t> buffer(length);
+
+    ros::serialization::OStream ostream(&buffer[0], length);
+    ros::serialization::Serializer<topic_tools::ShapeShifter>::write(ostream, request_message);
+    write_fn_(buffer,topic_id_);
+
+    //wait for the response
+    int cnt = 0;
+    while(!get_response_)
+      {
+        ros::Duration(0.01).sleep();
+
+        if (cnt++ > 500) // hard-coding
+          {
+            ROS_WARN_STREAM(service_server_.getService() << ": no response!!");
+            return false;
+          }
+      }
+
+    response_message = response_message_;
+
+    ROS_ERROR_STREAM("OK!!");
+    return true;
+  }
+
+  void response_handle(ros::serialization::IStream stream) {
+    ros::serialization::Serializer<topic_tools::ShapeShifter>::read(stream, response_message_);
+    ROS_DEBUG_STREAM("service receive " << service_server_.getService() << " response");
+    get_response_ = true;
+  }
+
+private:
+  //topic_tools::ShapeShifter request_message_;
+  topic_tools::ShapeShifter response_message_;
+  ros::ServiceServer service_server_;
+  boost::function<void(std::vector<uint8_t>& buffer, const uint16_t topic_id)> write_fn_;
+  std::string service_md5_;
+  std::string request_message_md5_;
+  std::string response_message_md5_;
+  uint16_t topic_id_;
+
+  bool get_response_;
+};
+
+typedef boost::shared_ptr<ServiceServer> ServiceServerPtr;
+
 }  // namespace
 
 #endif  // ROSSERIAL_SERVER_TOPIC_HANDLERS_H
