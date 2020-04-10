563cc630a4dee5b2f415acd8fbb04b4f308aefa7
Hermann von Kleist <hermann.von_kleist@turag.de>
2020-04-10 19:16:41 +0200

    Graceful shutdown
    Call io_service.stop() when ros::ok() returns false

--- rosserial_server/include/rosserial_server/session.h
+++ rosserial_server/include/rosserial_server/session.h
@@ -61,7 +61,8 @@ class Session : boost::noncopyable
 {
 public:
   Session(boost::asio::io_service& io_service)
-    : socket_(io_service),
+    : io_service_(io_service),
+      socket_(io_service),
       sync_timer_(io_service),
       require_check_timer_(io_service),
       ros_spin_timer_(io_service),
@@ -165,6 +166,10 @@ private:
       ros_spin_timer_.expires_from_now(ros_spin_interval_);
       ros_spin_timer_.async_wait(boost::bind(&Session::ros_spin_timeout, this,
                                              boost::asio::placeholders::error));
+    }
+    else
+    {
+      io_service_.stop();
     }
   }
 
@@ -482,6 +487,7 @@ private:
     set_sync_timeout(timeout_interval_);
   }
 
+  boost::asio::io_service& io_service_;
   Socket socket_;
   AsyncReadBuffer<Socket> async_read_buffer_;
   enum { buffer_max = 1023 };
