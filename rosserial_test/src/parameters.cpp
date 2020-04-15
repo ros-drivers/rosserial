1cbc6963d6841b927dd6c28850261e56149d1463
Hermann von Kleist <hermann.von_kleist@turag.de>
2020-04-15 20:27:30 +0200

    Added tests for list parameters

--- rosserial_test/src/parameters.cpp
+++ rosserial_test/src/parameters.cpp
@@ -38,6 +38,39 @@ TEST_F(SingleClientFixture, single_string_param) {
   EXPECT_STREQ(textbuf, "sample text");
 }
 
+TEST_F(SingleClientFixture, array_bool_param) {
+  client_nh.initNode();
+  bool array[2] = {false, true};
+  client_nh.getParam("bool_array", array);
+  EXPECT_EQ(array[0], true);
+  EXPECT_EQ(array[1], false);
+}
+
+TEST_F(SingleClientFixture, array_int_param) {
+  client_nh.initNode();
+  int array[2] = {0, 0};
+  client_nh.getParam("int_array", array);
+  EXPECT_EQ(array[0], 1);
+  EXPECT_EQ(array[1], 2);
+}
+
+TEST_F(SingleClientFixture, array_float_param) {
+  client_nh.initNode();
+  float array[2] = {0.f, 0.f};
+  client_nh.getParam("float_array", array);
+  EXPECT_EQ(array[0], 0.5f);
+  EXPECT_EQ(array[1], 4.0f);
+}
+
+TEST_F(SingleClientFixture, array_string_param) {
+  client_nh.initNode();
+  char textbuf[20][2] = {"", ""};
+  char* buf_ptr[2] = {textbuf[0], textbuf[1]};
+  client_nh.getParam("string_array", buf_ptr);
+  EXPECT_STREQ(textbuf[0], "sample text");
+  EXPECT_STREQ(textbuf[1], "more text");
+}
+
 int main(int argc, char **argv){
   ros::init(argc, argv, "test_publish_subscribe");
   ros::start();
