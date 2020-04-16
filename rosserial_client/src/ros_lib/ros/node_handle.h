1ab6304a1dc9bf8e30f4642346608e6694c4c005
Mike Purvis <mpurvis@clearpath.ai>
2020-04-16 20:09:15 +0000

    Use nullptr consistently.

--- rosserial_client/src/ros_lib/ros/node_handle.h
+++ rosserial_client/src/ros_lib/ros/node_handle.h
@@ -118,8 +118,8 @@ protected:
   uint8_t message_in[INPUT_SIZE] = {0};
   uint8_t message_out[OUTPUT_SIZE] = {0};
 
-  Publisher * publishers[MAX_PUBLISHERS];
-  Subscriber_ * subscribers[MAX_SUBSCRIBERS];
+  Publisher * publishers[MAX_PUBLISHERS] = {nullptr};
+  Subscriber_ * subscribers[MAX_SUBSCRIBERS] {nullptr};
 
   /*
    * Setup Functions
